d7b728b586f7621966399615ff342b1e53b5b1bc
Carlos Costa <jr.carlosad@gmail.com>
2018-07-20 14:50:05 -0300

    feat: Aplicando filtro de search para  listagem, usando o Pipe

---
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Photo } from '../photo/photo.model';
+
+@Pipe({
+  name: 'filterByDescription'
+})
+export class FilterByDescriptionPipe implements PipeTransform {
+  transform(photos: Photo[], descriptionQuery: string): Photo[] {
+    descriptionQuery = descriptionQuery.trim().toLocaleLowerCase();
+
+    if (descriptionQuery) {
+      return photos.filter(photo =>
+        photo.description.toLocaleLowerCase().includes(descriptionQuery)
+      );
+    } else {
+      return photos;
+    }
+  }
+}
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -11,6 +11,7 @@ import { PhotoService } from '../photo/photo.service';
 })
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
+  filter = '';
 
   constructor(
     private photoService: PhotoService,
