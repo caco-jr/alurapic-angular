3aacad7d45d8b517860a50a35f8c5feff4fbbd8c
Carlos Costa <jr.carlosad@gmail.com>
2018-07-20 00:37:06 -0300

    chore: Iniciando o projeto, e implementando sistema de Rotas

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+
+@NgModule({
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
+  exports: [],
+  imports: [HttpClientModule, CommonModule]
+})
+export class PhotosModule {}
