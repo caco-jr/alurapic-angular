6e78167aa3d8ac36a3476913ebf9dd25964735a1
Carlos Costa <jr.carlosad@gmail.com>
2018-07-23 15:20:04 -0300

    feat: Criando componente Card E utilizando o ng-content

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,6 +6,7 @@ import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from '../../shared/components/card/card.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,6 @@ import { PhotoModule } from '../photo/photo.module';
     LoadButtonComponent,
     FilterByDescriptionPipe
   ],
-  imports: [CommonModule, PhotoModule]
+  imports: [CommonModule, PhotoModule, CardModule]
 })
 export class PhotoListModule {}
